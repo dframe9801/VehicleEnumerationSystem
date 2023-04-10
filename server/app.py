058f86f08c09bf645e622ff223963c18f56d0d3a
Camron Walsh <camronwalsh@gmail.com>
2023-04-10 11:52:41 -0400

    Added OpenCV video capture

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 from flask_pymongo import PyMongo
-
+import cv2
 
 # configuration
 DEBUG = True
@@ -10,12 +10,27 @@ DEBUG = True
 app = Flask(__name__)
 app.config.from_object(__name__)
 
-mongodb_client = PyMongo(app, uri="mongodb://localhost:27017/VehicleEnumerationSystem")
-db = mongodb_client.db
-
 # enable CORS
 CORS(app, resources={r'/*': {'origins': '*'}})
 
+# database
+mongodb_client = PyMongo(app, uri="mongodb://localhost:27017/VehicleEnumerationSystem")
+db = mongodb_client.db
+
+# streaming 
+camera = cv2.VideoCapture("rtsp://192.168.1.178:554/VESTest")
+
+
+def gen_frames():  
+    while True:
+        success, frame = camera.read()  # read the camera frame
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
 
 # sanity check route
 @app.route('/ping', methods=['GET'])
