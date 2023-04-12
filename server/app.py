7ea54a845e8104510dc22ebe9ecac77f380a3f30
Camron Walsh <camronwalsh@gmail.com>
2023-04-12 12:35:35 -0400

    Implemented Vehicle Detection for test video

--- server/app.py
+++ server/app.py
@@ -24,9 +24,12 @@ db = mongodb_client.db
 tracker = EuclideanDistTracker()
 
 # streaming 
-camera = cv2.VideoCapture("rtsp://192.168.1.178")
-camera.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+# camera = cv2.VideoCapture("rtsp://192.168.1.178")
+# camera.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+# camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+
+# test video
+camera = cv2.VideoCapture("highway.mp4")
 
 # Object detection from Stable camera
 object_detector = cv2.createBackgroundSubtractorMOG2(
@@ -39,13 +42,49 @@ def video_feed():
     if not success:
         return jsonify({'success': False})
     
+    # ret, buffer = cv2.imencode('.jpg', frame,  [int(cv2.IMWRITE_JPEG_QUALITY), jpeg_quality])
+
+    height, width, _ = frame.shape
+
+    # Extract Region of interest
+    roi = frame[340: 720, 500: 800]
+
+    # 1. Object Detection
+    mask = object_detector.apply(roi)
+    _, mask = cv2.threshold(mask, 254, 255, cv2.THRESH_BINARY)
+    contours, _ = cv2.findContours(
+        mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    detections = []
+    for cnt in contours:
+        # Calculate area and remove small elements
+        area = cv2.contourArea(cnt)
+        if area > 100:
+            # cv2.drawContours(roi, [cnt], -1, (0, 255, 0), 2)
+            x, y, w, h = cv2.boundingRect(cnt)
+
+            detections.append([x, y, w, h])
+
+    # 2. Object Tracking
+    boxes_ids = tracker.update(detections)
+    for box_id in boxes_ids:
+        x, y, w, h, id = box_id
+        cv2.putText(roi, str(id), (x, y - 15),
+                    cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)
+        cv2.rectangle(roi, (x, y), (x + w, y + h), (0, 255, 0), 3)
+
+    # cv2.imshow("roi", roi)
+    # cv2.imshow("Frame", frame)
+    # cv2.imshow("Mask", mask)
+
+    key = cv2.waitKey(30)
+    if key == 27:
+        pass
+
     jpeg_quality = 90
     ret, buffer = cv2.imencode('.jpg', frame,  [int(cv2.IMWRITE_JPEG_QUALITY), jpeg_quality])
-    if ret:
-        frame_base64 = base64.b64encode(buffer.tobytes(), altchars=None).decode('utf-8')
-        return jsonify({'success': True, 'frame': frame_base64})
-    else:
-        return jsonify({'success': False, 'error': 'Failed to encode the camera frame'})
+
+    frame_base64 = base64.b64encode(buffer.tobytes(), altchars=None).decode('utf-8')
+    return jsonify({'success': True, 'frame': frame_base64})
 
 
 if __name__ == '__main__':
