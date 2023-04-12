05091bc2657407d01af9ec23229738156085f4bc
Camron Walsh <camronwalsh@gmail.com>
2023-04-11 21:48:53 -0400

    Seperated Graph and Webcam components

--- client/src/components/WebCam.vue
+++ client/src/components/WebCam.vue
@@ -1,39 +1,14 @@
 <template>
-  <div class="WebCam">
-    <div class="container text-center">
-      <div class="row">
-        <div class="col-2 border">
-          <div class="counter-container">
-            <p>Counter</p>
-            <div class="counter-box">
-              <span class="counter">0</span>
-            </div>
-          </div>
-        </div>
-        <div class="col border">
-          Stream
-          <img  v-if="frameSrc" :src="frameSrc" alt="Video feed" class="img-thumbnail" />
-        </div>
-      </div>
-      <div class="row">
-        <div class="col border">
-          <CPMGraph />
-        </div>
-        <div class="col border">
-          <CPMGraph />
-        </div>
-      </div>
-    </div>
+  <div class="WebCam p-2">
+    <img  v-if="frameSrc" :src="frameSrc" alt="Video feed" class="img-thumbnail" />
   </div>
 </template>
 
 <script>
 import axios from 'axios';
-import CPMGraph from './CarsPerMinGraph.vue';
 
 export default {
   name: 'WebCam',
-  components: { CPMGraph },
   data() {
     return {
       frameSrc: null,
@@ -62,27 +37,5 @@ export default {
 </script>
 
 <style scoped>
-.counter-box {
-    display: inline-flex;
-    justify-content: center;
-    align-items: center;
-    width: 100px;
-    height: 100px;
-    border: 2px solid #333;
-    border-radius: 5px;
-    background-color: #f7f7f7;
-}
 
-.counter {
-    font-size: 32px;
-    font-weight: bold;
-    color: #333;
-}
-
-.counter-container {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    padding: 10px;
-}
 </style>
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,18 +1,68 @@
 <template>
   <div class="home">
-    <WebCam/>
+    <div class="container text-center">
+      <div class="row">
+        <div class="col-2 border">
+          <div class="counter-container">
+            <p>Counter</p>
+            <div class="counter-box">
+              <span class="counter">0</span>
+            </div>
+          </div>
+        </div>
+        <div class="col border">
+          <WebCam />
+        </div>
+      </div>
+      <div class="row">
+        <div class="col border">
+          <CPMGraph />
+        </div>
+        <div class="col border">
+          <CPMGraph />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import WebCam from '@/components/WebCam.vue';
+import CPMGraph from '@/components/CarsPerMinGraph.vue';
 
 export default {
   name: 'HomeView',
   components: {
     WebCam,
+    CPMGraph,
 
   },
 };
 </script>
+
+<style>
+.counter-box {
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    width: 100px;
+    height: 100px;
+    border: 2px solid #333;
+    border-radius: 5px;
+    background-color: #f7f7f7;
+}
+
+.counter {
+    font-size: 32px;
+    font-weight: bold;
+    color: #333;
+}
+
+.counter-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 10px;
+}
+</style>
